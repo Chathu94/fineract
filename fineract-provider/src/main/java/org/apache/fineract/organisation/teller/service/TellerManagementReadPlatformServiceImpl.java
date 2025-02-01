9252552912ef4f1d779f17d4c4fa3ada05f5a9bc
Hashan Chathuranga <Hashan3SR@gmail.com>
2025-02-02 02:21:48 +0530

    changes

--- fineract-provider/src/main/java/org/apache/fineract/organisation/teller/service/TellerManagementReadPlatformServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/organisation/teller/service/TellerManagementReadPlatformServiceImpl.java
@@ -698,7 +698,7 @@ public class TellerManagementReadPlatformServiceImpl implements TellerManagement
             sqlBuilder.append(" left join m_account_transfer_transaction acnttrans1 ");
             sqlBuilder.append(" on acnttrans1.from_loan_transaction_id = loan_txn.id ");
             sqlBuilder.append(" left join m_account_transfer_transaction acnttrans2 ");
-            sqlBuilder.append(" on acnttrans2.to_loan_transaction_id = loan_txn.id) ");
+            sqlBuilder.append(" on acnttrans2.to_loan_transaction_id = loan_txn.id ");
 
             return sqlBuilder.toString();
         }
