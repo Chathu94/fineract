fcadf63e970dffcb4d5b7cc9801aceb21c7c2492
Hashan Chathuranga <Hashan3SR@gmail.com>
2026-01-07 12:42:29 +0530

    refactor: optimize interest posting for active savings accounts using pagination

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/service/SavingsSchedularServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/service/SavingsSchedularServiceImpl.java
@@ -18,9 +18,11 @@
  */
 package org.apache.fineract.portfolio.savings.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.fineract.infrastructure.core.service.DateUtils;
+import org.apache.fineract.infrastructure.core.service.RoutingDataSource;
 import org.apache.fineract.infrastructure.jobs.annotation.CronTarget;
 import org.apache.fineract.infrastructure.jobs.exception.JobExecutionException;
 import org.apache.fineract.infrastructure.jobs.service.JobName;
@@ -30,21 +32,25 @@ import org.apache.fineract.portfolio.savings.domain.SavingsAccountRepositoryWrap
 import org.apache.fineract.portfolio.savings.domain.SavingsAccountStatusType;
 import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SavingsSchedularServiceImpl implements SavingsSchedularService {
 
+    private final JdbcTemplate jdbcTemplate;
     private final SavingsAccountAssembler savingAccountAssembler;
     private final SavingsAccountWritePlatformService savingsAccountWritePlatformService;
     private final SavingsAccountRepositoryWrapper savingAccountRepositoryWrapper;
     private final SavingsAccountReadPlatformService savingAccountReadPlatformService;
 
     @Autowired
-    public SavingsSchedularServiceImpl(final SavingsAccountAssembler savingAccountAssembler,
-            final SavingsAccountWritePlatformService savingsAccountWritePlatformService,
-            final SavingsAccountRepositoryWrapper savingAccountRepositoryWrapper,
-            final SavingsAccountReadPlatformService savingAccountReadPlatformService) {
+    public SavingsSchedularServiceImpl(final RoutingDataSource dataSource, final SavingsAccountAssembler savingAccountAssembler,
+                                       final SavingsAccountWritePlatformService savingsAccountWritePlatformService,
+                                       final SavingsAccountRepositoryWrapper savingAccountRepositoryWrapper,
+                                       final SavingsAccountReadPlatformService savingAccountReadPlatformService) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
         this.savingAccountAssembler = savingAccountAssembler;
         this.savingsAccountWritePlatformService = savingsAccountWritePlatformService;
         this.savingAccountRepositoryWrapper = savingAccountRepositoryWrapper;
@@ -54,22 +60,47 @@ public class SavingsSchedularServiceImpl implements SavingsSchedularService {
     @CronTarget(jobName = JobName.POST_INTEREST_FOR_SAVINGS)
     @Override
     public void postInterestForAccounts() throws JobExecutionException {
-        final List<SavingsAccount> savingsAccounts = this.savingAccountRepositoryWrapper.findSavingAccountByStatus(SavingsAccountStatusType.ACTIVE
-                .getValue());
+        List<Long> savingsAccounts = new ArrayList<>();
         StringBuffer sb = new StringBuffer();
-        for (final SavingsAccount savingsAccount : savingsAccounts) {
-            try {
-                this.savingAccountAssembler.assignSavingAccountHelpers(savingsAccount);
-                boolean postInterestAsOn = false;
-                LocalDate transactionDate = null;
-                this.savingsAccountWritePlatformService.postInterest(savingsAccount, postInterestAsOn, transactionDate);
-            } catch (Exception e) {
-                Throwable realCause = e;
-                if (e.getCause() != null) {
-                    realCause = e.getCause();
+
+        int pageNumber = 0;
+        int pageSize = 1000;
+
+        savingsAccounts = this.jdbcTemplate.queryForList("select id from m_savings_account where status_enum = ? limit ? offset ?", new Object[] {
+                SavingsAccountStatusType.ACTIVE.getValue(), pageSize, pageNumber * pageSize
+                }, Long.class);
+//                .savingAccountRepositoryWrapper.findSavingAccountByStatus(SavingsAccountStatusType.ACTIVE
+//                .getValue(), new PageRequest(pageNumber, pageSize));
+//        System.out.println("Total number of active savings accounts to post interest page " + pageNumber + " : " + savingsAccounts.size());
+
+        while (savingsAccounts.size() > 0) {
+
+            System.out.println("Total number of active savings accounts to post interest page " + pageNumber + " : " + savingsAccounts.size());
+            for (final Long savingsAccountId : savingsAccounts) {
+                try {
+                    System.out.println("Posting interest for Savings with id " + savingsAccountId);
+                    final SavingsAccount savingsAccount = this.savingAccountRepositoryWrapper
+                            .findOneWithNotFoundDetection(savingsAccountId);
+                    savingsAccount.loadLazyCollections();
+                    this.savingAccountAssembler.assignSavingAccountHelpers(savingsAccount);
+                    boolean postInterestAsOn = false;
+                    LocalDate transactionDate = null;
+                    this.savingsAccountWritePlatformService.postInterest(savingsAccount, postInterestAsOn, transactionDate);
+                } catch (Exception e) {
+                    Throwable realCause = e;
+                    if (e.getCause() != null) {
+                        realCause = e.getCause();
+                    }
+                    System.out.println("failed to post interest for Savings with id " + savingsAccountId + " with message "
+                            + realCause.getMessage());
+                    sb.append("failed to post interest for Savings with id " + savingsAccountId + " with message "
+                            + realCause.getMessage());
+                } finally {
+                    pageNumber++;
+                    savingsAccounts = this.jdbcTemplate.queryForList("select id from m_savings_account where status_enum = ? limit ? offset ?", new Object[] {
+                            SavingsAccountStatusType.ACTIVE.getValue(), pageSize, pageNumber * pageSize
+                    }, Long.class);
                 }
-                sb.append("failed to post interest for Savings with id " + savingsAccount.getId() + " with message "
-                        + realCause.getMessage());
             }
         }
         
